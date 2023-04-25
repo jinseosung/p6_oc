a97eb9794eea8162e19274dcaa59ba309713889d
Jinseo Sung <jinseosung15@gmail.com>
2023-04-25 15:49:50 +0200

    add React Router

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
-    <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
